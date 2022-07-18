50dc48e11f48ad1db2463f0934da9c1ee58ea7ed
srevanwar09 <srevanwar09@domain.com>
2022-07-19 00:20:34 +0530

    program are added in UC-4

--- GitAssignmentDay-3/GitAssignmentDay-3/Program.cs
+++ GitAssignmentDay-3/GitAssignmentDay-3/Program.cs
@@ -2,27 +2,26 @@
 {
     public static void Main(String[] Args)
     {
-        Console.WriteLine("Welcome to Employee Wage Computation Program");
-        int IS_PART_TIME = 1;            
-        int IS_FULL_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
+        const int IS_PART_TIME = 1;
+        const int IS_FULL_TIME = 2;
+        const int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
         Random random = new Random();
         int empCheck = random.Next(0, 3);
-        if (empCheck == IS_FULL_TIME)
+        switch (empCheck)
         {
-            empHrs = 8;
-        }
-        else if (empCheck == IS_PART_TIME)
-        {
-            empHrs = 4;
-        }
-        else
-        {
-            empHrs = 0;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
+                break;
         }
         empWage = empHrs * EMP_RATE_PER_HOUR;
-        Console.WriteLine("Emp Wage:" + empWage);
+        Console.WriteLine("Emp Wage: " + empWage);
     }
 }
