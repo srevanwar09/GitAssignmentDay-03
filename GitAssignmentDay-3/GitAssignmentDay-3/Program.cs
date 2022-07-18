fbc4a170d056c43ff247f34b65cb7e95abe1a124
srevanwar09 <srevanwar09@domain.com>
2022-07-19 00:46:19 +0530

    program are added in UC-6

--- GitAssignmentDay-3/GitAssignmentDay-3/Program.cs
+++ GitAssignmentDay-3/GitAssignmentDay-3/Program.cs
@@ -2,33 +2,36 @@
 {
     public static void Main(String[] Args)
     {
+		const int IS_PART_TIME = 1;
 		const int IS_FULL_TIME = 2;
 		const int NUM_OF_WORKING_DAYS = 20;
 		const int EMP_RATE_PER_HOUR = 20;
-		const int IS_PART_TIME = 1;
-		int empHrs = 0;
-		int empWage = 0;
-		int totalEmpWage = 0;
-		Random random = new Random();
-		for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++)
+		const int MAX_HRS_IN_MONTH = 100;
+		int empHrs;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		int totalEmpWage;
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 		{
+			totalWorkingDays++;
+			Random random = new Random();
 			int empCheck = random.Next(0, 3);
 			switch (empCheck)
 			{
-				case IS_PART_TIME:
-					empHrs = 4;
-					break;
 				case IS_FULL_TIME:
 					empHrs = 8;
+					break;
+				case IS_PART_TIME:
+					empHrs = 4;
 					break;
 				default:
 					empHrs = 0;
 					break;
 			}
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			totalEmpWage += empWage;
-			Console.WriteLine("Day "+day+" Emp Wage: " + empWage);
+			totalEmpHrs += empHrs;
+			Console.WriteLine("Day: " + totalWorkingDays + " Emp Hrs: " +empHrs);
 		}
+		totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 		Console.WriteLine("Total Emp Wage: " + totalEmpWage);
     }
 }
