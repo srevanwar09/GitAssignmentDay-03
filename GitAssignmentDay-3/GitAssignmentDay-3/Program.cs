0da683a5afb9ebcbf0f95d368280b8d9c6941ff7
srevanwar09 <srevanwar09@domain.com>
2022-07-18 20:31:14 +0530

    GitAssignmentDay-3 UC-1 added

---
+++ GitAssignmentDay-3/GitAssignmentDay-3/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    public static void Main(String[] Args)
+    {
+        Console.WriteLine("Welcome to Employee Wage Computation Program");
+        int IS_PART_TIME = 1;
+        Random random = new Random();
+        int empCheck = random.Next(0, 2);
+        if (empCheck == IS_PART_TIME)
+        {
+            Console.WriteLine("Employee is Present");
+        }
+        else
+        {
+            Console.WriteLine("Employee is Absent");
+        }
+    }
+}
